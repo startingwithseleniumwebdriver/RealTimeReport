5c8700c76809ecac33c386164a369cc4403331f6
Hong Shen <hshen@rbauction.com>
2020-01-06 15:16:45 -0800

    Update CreateXML with CData content handler. And the usage remains the same.

--- RealTimeReport/src/report/realtime/filehandler/CreateXML.java
+++ RealTimeReport/src/report/realtime/filehandler/CreateXML.java
@@ -1,6 +1,11 @@
 package report.realtime.filehandler;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -9,11 +14,13 @@ import javax.xml.bind.Marshaller;
 import org.testng.ISuite;
 import org.testng.ITestResult;
 
+
 import report.realtime.datahandler.DataMap;
 import report.realtime.result.ConsolidatedResult;
 
 public class CreateXML {
 
+	@SuppressWarnings("resource")
 	synchronized public static void writeXML(ISuite iSuite, ConsolidatedResult result) {
 		if (!new File("./" + FileNameConstants.ROOT_FOLDER).exists()) {
 			new File("./" + FileNameConstants.ROOT_FOLDER).mkdir();
@@ -21,21 +28,22 @@ public class CreateXML {
 		int suiteIndex = 0;
 		if (DataMap.suiteMap.containsKey(iSuite)) {
 			suiteIndex = DataMap.suiteMap.get(iSuite);
+		}		
+		String fileName = "./" + FileNameConstants.ROOT_FOLDER + "/" + FileNameConstants.XML_FILE_NAME + "-" + suiteIndex + ".xml";
+		if (new File(fileName).exists()) {
+			new File(fileName).delete();
 		}
-		if (new File("./" + FileNameConstants.ROOT_FOLDER + "/" + FileNameConstants.XML_FILE_NAME + "-" + suiteIndex
-				+ ".xml").exists()) {
-			new File("./" + FileNameConstants.ROOT_FOLDER + "/" + FileNameConstants.XML_FILE_NAME + "-" + suiteIndex
-					+ ".xml").delete();
-		}
-
 		JAXBContext jaxbContext;
 		try {
 			jaxbContext = JAXBContext.newInstance(ConsolidatedResult.class);
 			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+			StringWriter sw = new StringWriter();			
+			CDATAContentHandler cDataContentHandler = new CDATAContentHandler(sw, "UTF-8");			
 			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-			jaxbMarshaller.marshal(result, new File("./" + FileNameConstants.ROOT_FOLDER + "/"
-					+ FileNameConstants.XML_FILE_NAME + "-" + suiteIndex + ".xml"));
-		} catch (JAXBException e) {
+			jaxbMarshaller.marshal(result, cDataContentHandler);						
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName, false), "UTF8") ) ;
+			bw.write(sw.toString());			
+		} catch (JAXBException | IOException e) {
 			e.printStackTrace();
 		}
 	}
